8d42274822e7d4fe1684e9131272f66eed1fc67f
zzh3153687185 <150423312+zzh3153687185@users.noreply.github.com>
2023-12-01 18:20:06 +0800

    Create Start.py

---
+++ Game/Scenes/Start.py
@@ -0,0 +1,20 @@
+import Game.Map.Begin.TestMap
+from Core.MathUtil import *
+import random
+
+import pygame.draw_py
+
+from Game.Characters.Humans.Player import *
+
+
+class TestScene(FightScene):
+    time_tot = 0
+    picture:Surface
+    def start(self):
+
+
+    def update(self, game_args: GameArgs):
+
+
+    def draw(self, surface_manager: SurfaceManager):
+
