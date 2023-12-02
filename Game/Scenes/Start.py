1a40f9a94e639de36ab36d52bc7af02e3146d7ca
zzh3153687185 <150423312+zzh3153687185@users.noreply.github.com>
2023-12-02 17:27:12 +0800

    Update Start.py

--- Game/Scenes/Start.py
+++ Game/Scenes/Start.py
@@ -11,19 +11,18 @@ from Game.Characters.Humans.Player import *
 class Start(Scene):
     time_tot = 0
     picture: Surface
-    picture: Button
+    picture_2: Surface
 
     def start(self):
         self.picture=load_image('BackGrounds\\main.jpg')
-        self.picture=load_image('BackGrounds\\')
+        self.picture_2=load_image('BackGrounds\\')
     def update(self, game_args: GameArgs):
         super().update(game_args) # 引擎内部每一帧都必须做的事情
 
-    def draw(self, surface_manager: SurfaceManager,button_manager:SurfaceManager):
+    def draw(self, surface_manager: SurfaceManager):
         super().draw(surface_manager) # 引擎内部每一帧都必须做的事情
-        super().draw(button_manager)
         rec1 = vec2(0, 0)
         rec2 = vec2(540, 400)
 
         surface_manager.screen.blit(self.picture, rec1) # 把图片画到指定的矩形区域
-        button_manager.screen.blit(self.picture, rec2)
+        surface_manager.screen.blit(self.picture_2, rec2)
